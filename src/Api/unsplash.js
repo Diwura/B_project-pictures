ca87204d94bfb53ddb2b6463bd8645a53acc5016
Diwura <boluroajayia@gmail.com>
2021-05-28 09:42:51 +0100

    initial commit

---
+++ src/Api/unsplash.js
@@ -0,0 +1,10 @@
+   import axios from 'axios';
+
+   export default axios.create({
+       baseURL: 'https://api.unsplash.com',
+       headers:{
+        Authorization: 'Client-ID KuRqNkV5RrAmV4x96HIlBdg3aOR4xdirIvT_dT3JVAs'
+        
+    }
+
+   });   
